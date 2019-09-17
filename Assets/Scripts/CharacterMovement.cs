6ea9ce41849bed0b28f2ae246a5c293ec8b4791c
reacotay <anton.hallqvist1@gmail.com>
2019-09-17 09:49:38 +0200

    Movement and Camera

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    //Camera cam;
+
+    public float speed;
+
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
+
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
