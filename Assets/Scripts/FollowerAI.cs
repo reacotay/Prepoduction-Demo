d03ad589fc4748e705027b8c26ce1c2aba857c82
reacotay <anton.hallqvist1@gmail.com>
2019-09-18 14:21:48 +0200

    Companion AI 1.0
    First implementation of the companion AI.

---
+++ Assets/Scripts/FollowerAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerAI : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed;
+    public float minDistance;
+    public float maxDistance;
+    public bool moveTowards;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) > maxDistance)
+            moveTowards = true;
+
+        if (Vector3.Distance(target.position, transform.position) < minDistance)
+            moveTowards = false;
+
+        if(moveTowards)
+        {
+            transform.LookAt(target.transform);
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed);
+        }
+        
+    }
+}
