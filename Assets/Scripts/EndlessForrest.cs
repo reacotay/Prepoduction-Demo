7536bc446173badd8a5c2046e6237ab76b8107c6
Aleskandor <37109130+Aleskandor@users.noreply.github.com>
2019-10-07 12:55:06 +0200

    Endless Forrest (FIX)
    Added the forgotten script needed for the forrest.

---
+++ Assets/Scripts/EndlessForrest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessForrest : MonoBehaviour
+{
+    private Vector3 tempPosition;
+    public GameObject player;
+    private BoxCollider boxCollider;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 playerPosition = player.transform.position;
+        tempPosition = Vector3.zero;
+
+        if (playerPosition.x <= transform.position.x - boxCollider.size.x / 2)
+            tempPosition += new Vector3(boxCollider.size.x, 0, 0);
+        else if (playerPosition.x >= transform.position.x + boxCollider.size.x / 2)
+            tempPosition += new Vector3(-boxCollider.size.x, 0, 0);
+
+        if (playerPosition.z <= transform.position.z - boxCollider.size.z / 2)
+            tempPosition += new Vector3(0, 0, boxCollider.size.z);
+        else if (playerPosition.z >= transform.position.z + boxCollider.size.z / 2)
+            tempPosition += new Vector3(0, 0, -boxCollider.size.z);
+
+        if (tempPosition != Vector3.zero)
+        {
+            CharacterMovement cMove = player.GetComponent<CharacterMovement>();
+            cMove.Locked = true;
+            player.gameObject.transform.position += tempPosition;
+        }
+
+        tempPosition = Vector3.zero;
+    }
+}
